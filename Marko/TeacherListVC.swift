e166c3cc3b61dde7b0c9d5d6ff4d48db9b5806d9
Ivan Yashchenko <ivan.profitx@gmail.com>
2025-08-21 01:17:57 +0300

    Initial commit

---
+++ Marko/TeacherListVC.swift
@@ -0,0 +1,16 @@
+//
+//  TeacherVC.swift
+//  Marko
+//
+//  Created by Ivan on 20.08.2025.
+//
+
+import UIKit
+
+class TeacherListVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
