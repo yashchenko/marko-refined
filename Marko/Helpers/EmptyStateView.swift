86e5946b053a6cb2bd271ea486da9d1fe26e7f39
Ivan Yashchenko <ivan.profitx@gmail.com>
2026-06-11 15:19:10 +0300

    fix(MAR-180): Finalize native Auto Layout for EmptyState and fix view lifecycle in LessonsVC

--- Marko/Helpers/EmptyStateView.swift
+++ Marko/Helpers/EmptyStateView.swift
@@ -32,15 +32,6 @@ class EmptyState: UIView {
         return label
     }()
     
-    
-    let subtitleLabel: UILabel = {
-        let label = UILabel()
-        
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
     // MARK: - Init
     
     override init(frame: CGRect) {
@@ -60,15 +51,14 @@ class EmptyState: UIView {
         addSubviews(views: [
         
             imageEmpty,
-            mainLabel,
-            subtitleLabel
+            mainLabel
         
         ])
         
         NSLayoutConstraint.activate([
             
-            imageEmpty.heightAnchor.constraint(equalToConstant: 150),
-            imageEmpty.widthAnchor.constraint(equalToConstant: 150),
+            imageEmpty.heightAnchor.constraint(equalToConstant: 200),
+            imageEmpty.widthAnchor.constraint(equalToConstant: 200),
             
             imageEmpty.topAnchor.constraint(equalTo: topAnchor, constant: 40),
             imageEmpty.centerXAnchor.constraint(equalTo: centerXAnchor)
@@ -78,26 +68,21 @@ class EmptyState: UIView {
         NSLayoutConstraint.activate([
         
             mainLabel.topAnchor.constraint(equalTo: imageEmpty.bottomAnchor, constant: 10),
+            
            
             mainLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
             mainLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
-            
-            subtitleLabel.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 20),
-            subtitleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 10),
-            subtitleLabel.heightAnchor.constraint(equalToConstant: 100),
-            subtitleLabel.widthAnchor.constraint(equalToConstant: 200)
+        
         
         ])
         
        
     }
     
-    func configure(image: String, mainLabel: String, subtitleLabel: String) {
+    func configure(image: String, mainLabel: String) {
                 
         imageEmpty.image = UIImage(named: image)
         self.mainLabel.text = mainLabel
-        self.subtitleLabel.text = subtitleLabel
-        
     }
     
 }
